411253a307a24d7669f8144c789ff668188bab7f
Jyothi <22mh1a05b5@acoe.edu.in>
2024-12-16 19:21:28 +0530

    Kth_element_of_2_arrays.cpp

---
+++ Kth_element_of_2_arrays.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+        // code here
+         int n = a.size();
+        int m = b.size();
+        int i = 0, j = 0, count = 0;
+        
+        // Merge arrays until we reach the k-th element
+        while (i < n && j < m) {
+            if (a[i] < b[j]) {
+                count++;
+                if (count == k) return a[i];
+                i++;
+            } else {
+                count++;
+                if (count == k) return b[j];
+                j++;
+            }
+        }
+
+        // If elements remain in array a
+        while (i < n) {
+            count++;
+            if (count == k) return a[i];
+            i++;
+        }
+
+        // If elements remain in array b
+        while (j < m) {
+            count++;
+            if (count == k) return b[j];
+            j++;
+        }
+
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> a, b;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
