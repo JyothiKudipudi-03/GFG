094640a0f89636066e38434297d0b1a75fe860f4
Jyothi <22mh1a05b5@acoe.edu.in>
2024-12-26 20:24:07 +0530

    TwoPairsum.cpp

---
+++ TwoPairsum.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        unordered_set<int> seen; // To store elements seen so far
+        
+        for (int num : arr) {
+            int complement = target - num; // Compute the complement
+            if (seen.find(complement) != seen.end()) {
+                return true; // Found a pair
+            }
+            seen.insert(num); // Add the current number to the set
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.twoSum(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
