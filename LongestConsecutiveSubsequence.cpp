07be318339c745ef4bf324450516958b6eccb693
Jyothi <22mh1a05b5@acoe.edu.in>
2024-12-31 15:53:20 +0530

    LongestConsecutiveSubsequence.cpp

---
+++ LongestConsecutiveSubsequence.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Function to return length of longest subsequence of consecutive integers.
+    int longestConsecutive(vector<int>& arr) {
+        // Your code here
+        unordered_set<int> hashSet(arr.begin(), arr.end());
+        int longestStreak = 0;
+
+        for (int num : arr) {
+            // Check if it's the start of a sequence
+            if (hashSet.find(num - 1) == hashSet.end()) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // Count consecutive numbers
+                while (hashSet.find(currentNum + 1) != hashSet.end()) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                // Update the maximum length
+                longestStreak = max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.longestConsecutive(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
